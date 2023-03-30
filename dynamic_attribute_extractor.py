1264b474ffacb81d60eae675c41ec3df017a0e12
Adrian Rebmann <rebmann@informatik.uni-mannheim.de>
2023-03-30 09:25:04 +0200

    skeleton and basic functionality

--- dynamic_attribute_extractor.py
+++ dynamic_attribute_extractor.py
@@ -6,6 +6,9 @@ from bert_parser import label_utils
 from const import TERMS_FOR_MISSING
 
 
+missing_pattern = '|'.join(TERMS_FOR_MISSING)
+
+
 class DynamicAttributeExtractor:
 
     def __init__(self, events: DataFrame, objects: Dict[str, DataFrame], activity_key="Activity"):
@@ -25,21 +28,26 @@ class DynamicAttributeExtractor:
 
     def compute_candidate_dynamic_attributes(self):
         candidates = {}
+        name_matches = {}
+        co_cooccurrences = {}
         for att in self.events.columns:
             print(self.events.dtypes[att])
             if self.events.dtypes[att] != object:
                 continue
             if att not in self.objects.keys():
-                print(att)
+                # We go over the object types and check for events if object instances are present and which attribute values
+                # are filled at the same time
+                for obj_type in self.objects.keys():
+                    with_instances = self.events[self.events[obj_type].notna() &
+                                                 (~(self.events[obj_type].astype(str).str.contains(missing_pattern))) &
+                                                 (~(self.events[att].astype(str).str.contains(missing_pattern)))]
+                    print(att, obj_type, len(with_instances))
                 # get rows for which this attribute has a value
-                pattern = '|'.join(TERMS_FOR_MISSING)
-                cols_with_val = self.events[(~(self.events[att].astype(str).str.contains(pattern)))]
-                print(att)
+                #cols_with_val = self.events[(~(self.events[att].astype(str).str.contains(missing_pattern)))]
+                #print(att)
 
             col_name = label_utils.sanitize_label(att)
             for obj in self.clean_objects:
                 if self.label_parser.label_util.lemmatize_word(obj) in col_name and not obj == col_name:
                     print("found match ", col_name, obj)
-        for obj_type in self.clean_objects:
-            print(obj_type)
-            pass
+
